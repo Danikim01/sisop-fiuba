19cffc3c807df784e5aa930bac9176335e9415f2
nehuencabibbo <ncabibbo@fi.uba.ar>
2023-04-14 01:28:02 -0300

    Modified REDIR case on exec_cmd() in exec.c

--- shell/exec.c
+++ shell/exec.c
@@ -108,11 +108,14 @@ open_redir_fd(char *file, int flags)
 {
 	int fd = open(file,
 	              flags | O_CLOEXEC |
-	                      (flags == O_RDONLY ? 0 : O_CREAT | O_TRUNC),
+	                      ((flags == O_RDONLY || strcmp("&1", file) == 0)
+	                               ? 0
+	                               : O_CREAT | O_TRUNC),
 	              S_IRUSR | S_IWUSR);
 	if (fd == -1) {
-		printf_debug("Fallo open con file:\n", file);
+		fprintf_debug(stderr, "ERROR: Open failed:\n");
 	}
+
 	return fd;
 }
 
@@ -223,78 +226,74 @@ exec_cmd(struct cmd *cmd)
 	}
 
 	case REDIR: {
-		// changes the input/output/stderr flow
-		//
-		// To check if a redirection has to be performed
-		// verify if file name's length (in the execcmd struct)
-		// is greater than zero
-		//
-		// Your code here
 		r = (struct execcmd *) cmd;
 
-		printf("type: %d\n", r->type);
-		printf("pid: %d\n", r->pid);
-		printf("scmd: %s\n", r->scmd);
-		printf("argc: %d\n", r->argc);
-		printf("eargc: %d\n", r->eargc);
+		int input_fd = -1;
+		int output_fd = -1;
+		int error_fd = -1;
 
-		// Imprimir los elementos del arreglo argv
-		printf("argv:\n");
-		for (int i = 0; i < r->argc; i++) {
-			printf("\targv[%d]: %s\n", i, r->argv[i]);
-		}
-
-		// Imprimir los elementos del arreglo eargv
-		printf("eargv:\n");
-		for (int i = 0; i < r->eargc; i++) {
-			printf("\teargv[%d]: %s\n", i, r->eargv[i]);
-		}
-
-		printf("out_file: %s\n", r->out_file);
-		printf("in_file: %s\n", r->in_file);
-		printf("err_file: %s\n", r->err_file);
-
-
-		if (strlen(r->out_file) > 0 && strlen(r->err_file) == 0) {
-			int fd_abierto = open_redir_fd(r->out_file, O_RDWR);
-			printf("El fd abierto es %d\n", fd_abierto);
-			dup2(fd_abierto, 1);
-			close(fd_abierto);
-			execvp(r->argv[0], r->argv);
-			perror("execvp");
-		} else if (strlen(r->in_file) > 0) {
-			int fd_abierto = open_redir_fd(r->in_file, O_RDONLY);
-			printf("El fd abierto es%d\n", fd_abierto);
-			dup2(fd_abierto, 0);
-			close(fd_abierto);
-			execvp(r->argv[0], r->argv);
-			perror("execvp");
-		} else if (strlen(r->err_file) > 0 && strlen(r->out_file) > 0) {
-			int index = block_contains(r->err_file, '&');
-			printf("El index es %d\n", index);
-			if (index == 0) {
-				int fd = open_redir_fd(r->out_file, O_RDWR);
-				dup2(fd, STDOUT_FILENO);
-				dup2(fd, STDERR_FILENO);
-				close(fd);
-				execvp(r->argv[0], r->argv);
-				perror("execvp");
+		// Redirect input (stdin)
+		if (strlen(r->in_file) > 0) {
+			input_fd = open_redir_fd(r->in_file, O_RDONLY);
+			if (input_fd < 0) {
+				fprintf(stderr, "ERROR: Open failed:\n");
+				exit(EXIT_FAILURE);
 			}
-
-			int fd_abierto = open_redir_fd(r->err_file, O_RDWR);
-			printf("El fd abierto es %d\n", fd_abierto);
-			dup2(fd_abierto, 2);
-
-			int fd_abierto2 = open_redir_fd(r->out_file, O_RDWR);
-			printf("El fd abierto es %d\n", fd_abierto2);
-			dup2(fd_abierto2, 1);
-
-			close(fd_abierto);
-			close(fd_abierto2);
-			execvp(r->argv[0], r->argv);
-			perror("execvp");
 		}
-		_exit(1);
+
+		// Redirect output (stdout)
+		if (strlen(r->out_file) > 0) {
+			// O_WRONLY = write only
+			output_fd = open_redir_fd(r->out_file, O_WRONLY);
+
+			if (output_fd < 0) {
+				fprintf(stderr, "ERROR: Open failed:\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		// Redirect errors (stderr)
+		if (strlen(r->err_file) > 0) {
+			error_fd = open_redir_fd(r->err_file, O_RDWR);
+
+			if (error_fd < 0) {
+				fprintf(stderr, "ERROR: Open failed:\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (output_fd != -1 && error_fd != -1) {
+			dup2(output_fd, STDOUT_FILENO);
+			if (block_contains(r->err_file, '&') == 0) {
+				// Caso ls > out.txt 2>&1
+				dup2(output_fd, STDERR_FILENO);
+			} else {
+				// Caso ls > out.txt 2>err.txt
+				dup2(error_fd, STDERR_FILENO);
+			}
+			close(output_fd);
+			close(error_fd);
+
+		} else if (output_fd != -1) {
+			// Caso ls > out.txt
+			dup2(output_fd, STDOUT_FILENO);
+			close(output_fd);
+		} else if (error_fd != -1) {
+			// ls 2>err.txt
+			dup2(error_fd, STDERR_FILENO);
+			close(error_fd);
+		}
+
+		if (input_fd != -1) {
+			// Cualquiera de los casos de arriba mezclado con <,
+			// o exclusivamente <
+			dup2(input_fd, STDIN_FILENO);
+			close(input_fd);
+		}
+		execvp(r->argv[0], r->argv);
+		fprintf(stderr, "ERROR: execvp failed\n");
+		exit(EXIT_FAILURE);
+
 		break;
 	}
 
