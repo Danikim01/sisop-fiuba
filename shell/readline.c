5aadb5d84bf0a44ee132889f39d0e58344437185
Regazzoli Ignacio <ignacioregazzoli@gmail.com>
2023-04-22 09:48:51 -0300

    fix error with readline

--- shell/readline.c
+++ shell/readline.c
@@ -81,6 +81,7 @@ input_from_stdin(const char *prompt)
 {
 	char c;
 	int command_index = 0;
+	bool keep_reading = true;
 
 	set_input_mode();
 
@@ -89,96 +90,92 @@ input_from_stdin(const char *prompt)
 	fprintf(stdout, "%s", "$ ");
 #endif
 
-	while (true) {
+	while (keep_reading) {
 		assert(read(STDIN_FILENO, &c, 1) > 0);
-		if (c == CHAR_NEW_LINE) {
-			// Enter input
-			buffer[command_index++] = CHAR_NEW_LINE;
+
+		switch (c) {
+		case CHAR_NEW_LINE:
 			buffer[command_index] = '\0';
 			assert(write(STDOUT_FILENO, &c, 1) > 0);
-			reset_input_mode();
-			return strdup(buffer);
-		}
-
-		if (c == CHAR_EOF) {
-			// CTRL-d input
-			return NULL;
-		}
-
-		if (c == CHAR_DEL) {
-			// input "Backspace"
-			if (command_index == 0) {
-				// beginning
-				continue;
+			keep_reading = false;
+			break;
+		case CHAR_EOF:
+			keep_reading = false;
+			break;
+		case CHAR_DEL:
+			if (command_index > 0) {
+				delete_char();
+				buffer[command_index--] = '\0';
 			}
-
-			delete_char();
-			buffer[command_index--] = '\0';
-		}
-
-		if (c == CHAR_ESCSEQ) {
-			// checks if escape sequence is an arrow
-			char esc_seq;
-			assert(read(STDIN_FILENO, &esc_seq, 1) > 0);
-			if (esc_seq != '[') {
-				// we do nothing sicen its not an arrow
-			} else {
-				// now we check which of the 4 arrows is it
-				assert(read(STDIN_FILENO, &esc_seq, 1) > 0);
-
-				switch (esc_seq) {
-				case 'A':
-					// interact with history (get the previous command)
-					char *previous_command =
-					        get_previous_command();
-
-					if (previous_command != NULL) {
-						// Delete the current input in the buffer
-						while (command_index > 0) {
-							delete_char();
-							buffer[--command_index] =
-							        '\0';
-						}
-
-						// Copy the previous command into the buffer
-						strcpy(buffer, previous_command);
-						command_index =
-						        strlen(previous_command);
-
-						// Write the previous command to the terminal
-						assert(write(STDOUT_FILENO,
-						             previous_command,
-						             command_index) > 0);
-
-						// Free the memory allocated by strdup in get_previous_command
-						free(previous_command);
-					}
-					return "up";
-
-				case 'B':
-					// interact with history
-					return "down";
-
-				case 'C':
-					// interact with command
-					return "right";
-
-				case 'D':
-					// interact with command
-					return "left";
-				}
+			break;
+		case CHAR_ESCSEQ:
+			handle_escape_sequence(&command_index);
+			break;
+		default:
+			if (isprint(c)) {
+				assert(write(STDOUT_FILENO, &c, 1) > 0);
+				buffer[command_index++] = c;
 			}
-		}
-
-		if (isprint(c)) {
-			assert(write(STDOUT_FILENO, &c, 1) > 0);
-			buffer[command_index++] = c;
+			break;
 		}
 	}
 
 	reset_input_mode();
-	return buffer;
+
+	if (c == CHAR_EOF) {
+		return NULL;
+	} else {
+		return strdup(buffer);
+	}
 }
+
+void
+handle_escape_sequence(int *command_index)
+{
+	char esc_seq;
+	assert(read(STDIN_FILENO, &esc_seq, 1) > 0);
+	if (esc_seq != '[') {
+		return;
+	}
+
+	assert(read(STDIN_FILENO, &esc_seq, 1) > 0);
+
+	switch (esc_seq) {
+	case 'A':
+		handle_up_arrow(command_index);
+		break;
+	case 'B':
+		// interact with history
+		break;
+	case 'C':
+		// interact with command
+		break;
+	case 'D':
+		// interact with command
+		break;
+	}
+}
+
+void
+handle_up_arrow(int *command_index)
+{
+	char *previous_command = get_previous_command();
+
+	if (previous_command != NULL) {
+		while (*command_index > 0) {
+			delete_char();
+			buffer[--(*command_index)] = '\0';
+		}
+
+		strcpy(buffer, previous_command);
+		*command_index = strlen(previous_command);
+
+		assert(write(STDOUT_FILENO, previous_command, *command_index) > 0);
+
+		free(previous_command);
+	}
+}
+
 
 // reads a line from the standard input
 // and prints the prompt
