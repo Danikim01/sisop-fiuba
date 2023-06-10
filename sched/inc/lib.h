e7e46eedb4d8d9d7e6168b8b9dbabd6927b25c55
Danikim01 <dakim@fi.uba.ar>
2023-06-10 19:09:00 -0300

    make format

--- sched/inc/lib.h
+++ sched/inc/lib.h
@@ -81,9 +81,9 @@ envid_t sfork(void);  // Challenge!
 #define O_RDWR 0x0002    /* open for reading and writing */
 #define O_ACCMODE 0x0003 /* mask for above modes */
 
-#define O_CREAT 0x0100   /* create if nonexistent */
-#define O_TRUNC 0x0200   /* truncate to zero length */
-#define O_EXCL 0x0400    /* error if already exists */
-#define O_MKDIR 0x0800   /* create directory, not regular file */
+#define O_CREAT 0x0100 /* create if nonexistent */
+#define O_TRUNC 0x0200 /* truncate to zero length */
+#define O_EXCL 0x0400  /* error if already exists */
+#define O_MKDIR 0x0800 /* create directory, not regular file */
 
-#endif                   // !JOS_INC_LIB_H
+#endif  // !JOS_INC_LIB_H
